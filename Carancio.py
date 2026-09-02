37b97ac7afdb8eeb04cbcaf4c0f47fbd05028a4f
IvesCarancio <icarancio2031@wvc.pshs.edu.ph>
2026-09-02 14:14:58 +0800

    Add comments to clarify input and distance calculation

--- Carancio.py
+++ Carancio.py
@@ -1,12 +1,12 @@
 import math # imports the map library
 
-point_x1 = float(input("Please enter point_x1. ")) # 
-point_x2 = float(input( "Please enter point_x2. "))
-point_y1 = float(input("Please enter point y_1. " ))
-point_y2 = float(input( "Please enter point_y2. "))
+point_x1 = float(input("Please enter point_x1. ")) # This is the input for the first x coordinate.
+point_x2 = float(input( "Please enter point_x2. ")) # This is the input for the second x coordinate.
+point_y1 = float(input("Please enter point y_1. " )) # This is the input for the first y coordinate.
+point_y2 = float(input( "Please enter point_y2. ")) # This is the input for the second y coordinate.
 
-point_a = pow( point_x2 - point_x1, 2 )
-point_b = pow(point_y2 - point_y1, 2)
-distance = math.sqrt(point_a + point_b)
+point_a = pow( point_x2 - point_x1, 2 ) # this computes for the point a.
+point_b = pow(point_y2 - point_y1, 2) # this computes for the point b.
+distance = math.sqrt(point_a + point_b) # This computes the final distance.
 
-print ("Your distance is", distance)
+print ("Your distance is", distance) #This shows the final distance.
